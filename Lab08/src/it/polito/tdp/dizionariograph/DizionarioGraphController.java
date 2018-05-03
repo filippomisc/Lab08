07aff1aa7a6062f64c52a4a4a636a298c32752eb
JIMMY <jimmy@MacBook-Pro-di-JIMMY.local>
2018-05-04 00:06:53 +0200

    v1.0 creato controller e setModel

---
+++ Lab08/src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -0,0 +1,83 @@
+/**
+ * Sample Skeleton for 'DizionarioGraph.fxml' Controller Class
+ */
+
+package it.polito.tdp.dizionariograph;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class DizionarioGraphController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtNumberOfCharacters"
+    private TextField txtNumberOfCharacters; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtWords"
+    private TextField txtWords; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnGeneraGrafo"
+    private Button btnGeneraGrafo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnTrovaVicini"
+    private Button btnTrovaVicini; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnTrovaGrafo"
+    private Button btnTrovaGrafo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+	private Model m;
+
+    @FXML
+    void GeneraGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Reset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TrovaGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TrovaVicini(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtNumberOfCharacters != null : "fx:id=\"txtNumberOfCharacters\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtWords != null : "fx:id=\"txtWords\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGeneraGrafo != null : "fx:id=\"btnGeneraGrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnTrovaVicini != null : "fx:id=\"btnTrovaVicini\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnTrovaGrafo != null : "fx:id=\"btnTrovaGrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.m = model;
+	}
+}
---
+++ Lab08/src/it/polito/tdp/dizionariograph/Main.java
@@ -0,0 +1,40 @@
+package it.polito.tdp.dizionariograph;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("DizionarioGraph.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			
+			// Create here your model.
+			DizionarioGraphController controller = loader.getController();
+			Model model = new Model();
+			
+			controller.setModel(model);
+
+			// Assign here the model to the controller.
+			
+			
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
