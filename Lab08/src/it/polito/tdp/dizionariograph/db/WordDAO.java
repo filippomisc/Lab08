d2a22df60a716dffb649d70214399350419b70bf
mastr <mastr@172.22.83.25>
2018-05-25 19:50:02 +0200

    v.1.3 @ creato l'algoritmo per cercare nel database le parole simili è stato creato ma non funge @TODO corregere l'algoritmo

--- Lab08/src/it/polito/tdp/dizionariograph/db/WordDAO.java
+++ Lab08/src/it/polito/tdp/dizionariograph/db/WordDAO.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WordDAO {
-
+  
 	/*
 	 * Ritorna tutte le parole di una data lunghezza
 	 */
@@ -37,28 +37,62 @@ public class WordDAO {
 
 	public List<String> getAllWordsFixedLengthAndConnected(String p, int length) {
 		
-		String sql = "SELECT nome FROM parola WHERE LENGTH(nome)= ?;";
+		String sql = "SELECT nome FROM parola \n" + 
+				"WHERE nome \n" + 
+				"LIKE nome=? AND LENGTH(nome) = ?";
+		
 		List<String> parole = new ArrayList<String>();
 
 		try {
-			Connection conn = ConnectDB.getConnection();
-			PreparedStatement st = conn.prepareStatement(sql);
-			st.setInt(1, length);
-			ResultSet res = st.executeQuery();
+			
+			//copiare la parola in un array di char per dividere le lettere
+			char[] charParolaDaCercare = p.toCharArray();
+			
+			/**
+			 * fare un ciclo for per sostituire ad ogni lettera del char
+			 * uno '_'  per fare la query in sql
+			 */
+			for(int i=0; i<p.length(); i++) {
+				
+				//salviamo prima la letteera i-esima in una var temporanea
+				//per poi inserirla di nuovo ***
+				char temp = charParolaDaCercare[i];
+				
+						//sostituiamo ad ogni lettera i-esima il carattere'_'
+						charParolaDaCercare[i] = '_';
+						
+						//copiamo la parola che useremo nella query
+						String pDaCercare = String.copyValueOf(charParolaDaCercare);
+				
+				//***
+				charParolaDaCercare[i] = temp;
+				
+				
 
-			while (res.next()) {
-				parole.add(res.getString("nome"));
+				Connection conn = ConnectDB.getConnection();
+				PreparedStatement st = conn.prepareStatement(sql);
+				
+				st.setString(1, pDaCercare);
+				st.setInt(2, length);
+				
+				ResultSet res = st.executeQuery();
+				
+				while(res.next()) {
+					
+					String nome = res.getString("nome");
+					parole.add(nome);
+				}
+
+				
 			}
-
 			return parole;
-
+			
 		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Error Connection Database");
+				e.printStackTrace();
+				throw new RuntimeException("Error Connection Database");
 		}
 	}
-	
-	
-	
+				
+
 
 }
